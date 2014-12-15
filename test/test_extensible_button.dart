768162c0254aab6ddc7894a7a3a107dd70595a9a
GeReinhart <gerald.reinhart@gmail.com>
2014-12-15 23:43:13 +0100

    put back the CI green

--- test/test_extensible_button.dart
+++ test/test_extensible_button.dart
@@ -48,7 +48,7 @@ main() {
       });
 
       test('large button should show the text', (){
-        expect( largeButton.isButtonLabelVisible  , isFalse); 
+        expect( largeButton.isButtonLabelVisible  , isTrue); 
       });
       
     });
