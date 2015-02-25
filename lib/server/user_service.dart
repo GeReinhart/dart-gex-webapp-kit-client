d8a9f331b7a81dd1527f1a4ad6059cd5bf9f3e1b
GeReinhart <gerald.reinhart@gmail.com>
2015-02-25 13:25:16 +0100

    only one class for the service

--- lib/server/user_service.dart
+++ lib/server/user_service.dart
@@ -2,15 +2,16 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 part of gex_webapp_kit_server;
 
+@app.Group("/services")
+@Encode()
+class UserService extends MongoDbService<User> {
 
-class CrudUserService  {
+  UserService() : super("users");
+  UserService.withTargetCollection(String collectionName) : super(collectionName);
 
-  MongoDbService _mongoDbService ;
-  
-  CrudUserService(this._mongoDbService) ;
-
+  @app.Route("/login/:openId", methods: const [app.POST])
   Future<User> login(@Decode() User user) {
-    return _mongoDbService.findOne({"openId": user.openId}).then((user) {
+    return findOne({"openId": user.openId}).then((user) {
         if (user == null){
           return new User();
         }else{
@@ -19,13 +20,15 @@ class CrudUserService  {
     });
   } 
   
+  @app.Route("/register/:openId", methods: const [app.POST])
   Future<User> register(@Decode() User user) {
-    return _mongoDbService.insert(user).then((_) => user);
+    return insert(user).then((_) => user);
   }
   
   
+  @app.Route("/user/:openId", methods: const [app.GET])
   Future<User> get(String openId) {
-    return _mongoDbService.findOne({"openId": openId}).then((user) {
+    return findOne({"openId": openId}).then((user) {
       if (user == null){
         return new User();
       }else{
@@ -34,59 +37,23 @@ class CrudUserService  {
     });
   }
   
-  Future<List<User>> load() => _mongoDbService.find();
+  @app.Route("/users", methods: const [app.GET])
+  Future<List<User>> load() => find();
 
+  @app.Route("/user/:openId", methods: const [app.POST])
   Future<User> addOrUpdate(@Decode() User inputUser) {
-    return _mongoDbService.findOne({"openId": inputUser.openId}).then((existingUser) {
+    return findOne({"openId": inputUser.openId}).then((existingUser) {
       if (existingUser == null) {
-        return _mongoDbService.insert(inputUser).then((_) => inputUser);
+        return insert(inputUser).then((_) => inputUser);
       }else{
-        return _mongoDbService.update({"openId": existingUser.openId},inputUser).then((_) => inputUser);
+        return update({"openId": existingUser.openId},inputUser).then((_) => inputUser);
       }
     });
   }
 
+  @app.Route("/user/:openId", methods: const [app.DELETE])
   Future<bool> delete(String openId) =>
-      _mongoDbService.remove(where.id(ObjectId.parse(openId))).then((_) => true);
+    remove(where.id(ObjectId.parse(openId))).then((_) => true);
 
 }
 
-@app.Group("/services")
-@Encode()
-class UserService {
-  
-  CrudUserService _service ;
-  
-  UserService(){
-    _service = new CrudUserService( new MongoDbService("users") ); 
-  }
-  
-  @app.Route("/login/:openId", methods: const [app.POST])
-    Future<User> login(@Decode() User user) {
-      return _service.login(user) ;
-    } 
-    
-    @app.Route("/register/:openId", methods: const [app.POST])
-    Future<User> register(@Decode() User user) {
-      return _service.register(user);
-    }
-    
-    
-    @app.Route("/user/:openId", methods: const [app.GET])
-    Future<User> get(String openId) {
-      return _service.get(openId);
-    }
-    
-    @app.Route("/users", methods: const [app.GET])
-    Future<List<User>> load() => _service.load();
-
-    @app.Route("/user/:openId", methods: const [app.POST])
-    Future<User> addOrUpdate(@Decode() User inputUser) {
-      return _service.addOrUpdate(inputUser);
-    }
-
-    @app.Route("/user/:openId", methods: const [app.DELETE])
-    Future<bool> delete(String openId) =>
-        _service.delete(openId);
-
-}
